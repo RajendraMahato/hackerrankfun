010f26714a0c6e2df6f2f4b5e6fa632d0b3095fa
Rajendra Mahato <rajendra.ccct@gmail.com>
2020-06-04 16:09:59 +0530

    normal commit

--- src/com/hackerrank/example/IceCreamParlor.java
+++ src/com/hackerrank/example/IceCreamParlor.java
@@ -1,4 +1,44 @@
 package com.hackerrank.example;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class IceCreamParlor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the number ofoperation do you want");
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+            System.out.println("enter the amount");
+            int m = scanner.nextInt();
+            System.out.println("enter the size of the array");
+            int n = scanner.nextInt();
+            System.out.println("enter " + n + " array element");
+            int a[] = new int[n];
+            for (int j = 0; j < n; j++) {
+                a[j] = scanner.nextInt();
+            }
+            int result[] = iceCreamParlor(m, a);
+            System.out.println(result[0] +" "+result[1]);
+            scanner.close();
+        }
+    }
+
+    private static int[] iceCreamParlor(int m, int[] a) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int result[] = new int[2];
+        for (int i = 0; i < a.length; i++) {
+            int x = a[i];
+            int y = m - x;
+            Integer j = map.get(y);//getting the index of value y from map
+            if (j != null) {
+                result[0] = j + 1; //in the question they have mentioned that use 1 based index,but we were using 0 based that is why doing +1
+                result[1] = i + 1;
+                break;
+            }
+            map.put(x, i);
+        }
+        return result;
+    }
 }
--- src/com/hackerrank/example/SeparateTheNumbers.java
+++ src/com/hackerrank/example/SeparateTheNumbers.java
@@ -1,4 +1,35 @@
 package com.hackerrank.example;
 
+import java.util.Scanner;
+
 public class SeparateTheNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter how many string you want to enter");
+        int n = scanner.nextInt();
+        System.out.println("enter string");
+        while (n-- > 0) {
+            String s = scanner.next();
+            separatNumber(s);
+        }
+        scanner.close();
+    }
+
+    static void separatNumber(String s) {
+        String subString = "";
+        boolean isValid = false;
+        for (int i = 0; i < s.length()/2; i++){
+            subString = s.substring(0, i);
+            long num = Long.parseLong(subString);
+            String validString = subString;//we will use this to append all string, then will chk if its valid string or not
+            while(validString.length() < s.length()){
+                validString += String.valueOf(num++);
+            }
+            if (s.equals(validString)){
+                isValid = true;
+                break;
+            }
+        }
+        System.out.println(isValid ? "Yes"+ subString : "No");
+    }
 }
